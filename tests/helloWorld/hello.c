28c30def992e53c48093eaec54e6947c3ddac9b6
binhkieudo <binhkieudo@gmail.com>
2024-01-31 12:33:55 +0900

    fix tiny

--- tests/helloWorld/hello.c
+++ tests/helloWorld/hello.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <stdint.h>
 #include <riscv-pk/encoding.h>
 #include "include/platform.h"
-#include "kprintf.h"
+// #include "kprintf.h"
+#define REG32(p, i)	((p)[(i) >> 2])
 
+static volatile uint32_t * const gpio = (void *)(GPIO_CTRL_ADDR);
 
 int main(void) {
   uint32_t gpio_val = 0x0000aaaa;
   REG32(gpio, GPIO_OUTPUT_EN) = 0x0000ffff;
   REG32(gpio, GPIO_OUTPUT_VAL) = 0x0000aaaa;
-  
-  REG32(uart, UART_REG_TXCTRL) = UART_TXEN;
-  kputs("Hello world from PHAM Lab\n");
 
   while (1) {
     for (int i = 0; i < 100000; i = i + 1);
